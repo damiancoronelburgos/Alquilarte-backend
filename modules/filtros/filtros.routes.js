2e84d427d6412c09e85c2418806b61a24113e7b6
AACubas <56238916+AACubas@users.noreply.github.com>
2025-05-24 00:06:31 -0300

    Update filtros.routes.js
    agregada la lógica de routes

--- modules/filtros/filtros.routes.js
+++ modules/filtros/filtros.routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const filtrosController = require("./filtros.controller");
 
+// Ruta de prueba original
 router.get("/", (req, res) => {
   res.send("Ruta funcionando correctamente");
 });
+
+// Nueva ruta para mostrar tareas filtradas en Pug
+router.get("/vista", filtrosController.filtrarYMostrar);
 
 module.exports = router;

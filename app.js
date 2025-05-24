336ccc600efaf191f5c8c11d7bc33b3b0fddf767
damiancoronelburgos <danielcoronelburgos1993@gmail.com>
2025-05-24 02:56:31 -0300

    cambios en vistas
    subo los cambios realizados en el menu y tareas

--- app.js
+++ app.js
@@ -5,54 +5,66 @@ const errorHandler = require('./middlewares/errorHandler');
 const app = express();
 const port = 3000;
 
+// Configuración del motor de vistas
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use(express.json());
 app.use(express.static('public'));
 
-// Usar el middleware propio para cada solicitud
+// Middleware propio
 app.use(requestLogger);
 
+// Rutas modulares
 const tareasRoutes = require("./modules/tareas/tareas.routes");
 const empleadosRoutes = require("./modules/empleados/empleados.routes");
 const filtrosRoutes = require("./modules/filtros/filtros.routes");
+const loginRoutes = require('./modules/login/login.routes'); // Descomentado y utilizado correctamente
 
-const loginRoutes = require('./modules/login/login.routes');
-app.use('/login', loginRoutes);
-
+app.use("/login", loginRoutes);
 app.use("/tareas", tareasRoutes);
 app.use("/empleados", empleadosRoutes);
 app.use("/filtros", filtrosRoutes);
 
-// Vista pug de empleados
+// Vistas Pug
+app.get('/menu', (req, res) => {
+  res.render('menu');
+});
+
 app.get('/empleados-vista', (req, res) => {
   res.render('empleados');
 });
 
-// Vista pug de tareas
+app.get('/filtros/vista', (req, res) => {
+  res.render('filtros');
+});
 
+app.get('/tareas/vista', (req, res) => {
+  res.render('tareas');
+});
 
-
-
-
+// Ruta raíz
 app.get("/", (req, res) => {
   res.send("¡Hola mundo!");
 });
 
+// Ruta para probar errores
 app.get('/error', (req, res) => {
   throw new Error('¡Este es un error de prueba!');
 });
 
-/* app.use((req, res, next) => {
+/* Middleware 404 personalizado (opcional)
+app.use((req, res, next) => {
   const err = new Error('Ruta no encontrada');
   err.status = 404;
   next(err);
-}); */
+});
+*/
 
 // Middleware de errores (siempre al final)
 app.use(errorHandler);
 
+// Iniciar servidor
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
